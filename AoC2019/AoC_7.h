1ea8c8a94a3dfdd3784b199a2ae5779570e18bb7
NickA <nicholasjalderton@gmail.com>
2019-12-10 18:14:05 +0000

    Also.... arrays

--- AoC2019/AoC_7.h
+++ AoC2019/AoC_7.h
@@ -11,32 +11,15 @@ namespace AoC_7 {
         std::vector<long long> permutations = { 0,1,2,3,4 };
         long long thrust = 0;
         do {
-            IntCodeCPU computer1(program);
-            IntCodeCPU computer2(program);
-            IntCodeCPU computer3(program);
-            IntCodeCPU computer4(program);
-            IntCodeCPU computer5(program);
-            computer1.Input(permutations[0]);
-            computer2.Input(permutations[1]);
-            computer3.Input(permutations[2]);
-            computer4.Input(permutations[3]);
-            computer5.Input(permutations[4]);
-            computer1.RunToInput();
-            computer1.Input(0);
-            computer1.RunToOutput();
-            computer2.RunToInput();
-            computer2.Input(computer1.Output());
-            computer2.RunToOutput();
-            computer3.RunToInput();
-            computer3.Input(computer2.Output());
-            computer3.RunToOutput();
-            computer4.RunToInput();
-            computer4.Input(computer3.Output());
-            computer4.RunToOutput();
-            computer5.RunToInput();
-            computer5.Input(computer4.Output());
-            computer5.RunToOutput();
-            thrust = std::max(thrust, computer5.Output());
+            std::vector<IntCodeCPU> cpus = {};
+            for (int i = 0; i < 5; i++) {
+                cpus.push_back(IntCodeCPU(program));
+                cpus[i].Input(permutations[i]);
+                cpus[i].RunToInput();
+                cpus[i].Input(i == 0 ? 0 : cpus[i - 1].Output());
+                cpus[i].RunToOutput();
+            }
+            thrust = std::max(thrust, cpus[4].Output());
         } while (std::next_permutation(permutations.begin(), permutations.end()));
         return thrust;
     }
@@ -45,34 +28,19 @@ namespace AoC_7 {
         std::vector<long long> permutations = { 5,6,7,8,9 };
         long long thrust = 0;
         do {
-            IntCodeCPU computer1(program);
-            IntCodeCPU computer2(program);
-            IntCodeCPU computer3(program);
-            IntCodeCPU computer4(program);
-            IntCodeCPU computer5(program);
-            computer1.Input(permutations[0]);
-            computer2.Input(permutations[1]);
-            computer3.Input(permutations[2]);
-            computer4.Input(permutations[3]);
-            computer5.Input(permutations[4]);
+            std::vector<IntCodeCPU> cpus = {};
+            for (int i = 0; i < 5; i++) {
+                cpus.push_back(IntCodeCPU(program));
+                cpus[i].Input(permutations[i]);
+            }
             long long comp1input = 0;
-            while (!computer5.halted) {
-                computer1.RunToInput();
-                computer1.Input(comp1input);
-                computer1.RunToOutput();
-                computer2.RunToInput();
-                computer2.Input(computer1.Output());
-                computer2.RunToOutput();
-                computer3.RunToInput();
-                computer3.Input(computer2.Output());
-                computer3.RunToOutput();
-                computer4.RunToInput();
-                computer4.Input(computer3.Output());
-                computer4.RunToOutput();
-                computer5.RunToInput();
-                computer5.Input(computer4.Output());
-                computer5.RunToOutput();
-                comp1input = computer5.Output();
+            while (!cpus[4].halted) {
+                for (int i = 0; i < 5; i++) {
+                    cpus[i].RunToInput();
+                    cpus[i].Input(i == 0 ? comp1input : cpus[i-1].Output());
+                    cpus[i].RunToOutput();
+                }
+                comp1input = cpus[4].Output();
             }
             thrust = std::max(thrust, comp1input);
         } while (std::next_permutation(permutations.begin(), permutations.end()));
